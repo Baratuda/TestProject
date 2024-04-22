abe97da895b614609f3d0ef3eaa41ab9d31d7df4
Egor <egorpifagop@gmail.com>
2024-04-22 17:40:44 +0300

    JS file change

--- static/js/classChanger.js
+++ static/js/classChanger.js
@@ -1,7 +1,12 @@
 // При загрузке страницы меняет стиль активного элемента
 var activeElementId = sessionStorage.getItem('activate');
 $('#'+activeElementId).children('a').css('border','3px solid rgb(20, 104, 150)');
-
+// Регулирует отступы меню 
+$('ul').each(function(index) {
+    var len = $(this).parents('ul').length;
+    var padding = 20+len*15 + 'px';
+    $(this).children('li').children('a').css('padding-left',padding);
+});
 // При загрузке страницы меняет все сохраненные классы в sessionStorage
 // и в зависимости от полученого класса закрывают или раскрывают меню или подменю 
 for(let key in sessionStorage) {
--- static/js/clickActions.js
+++ static/js/clickActions.js
@@ -4,17 +4,25 @@ $(document).ready(function () {
     // has-sub если меню закрывается и has-sub open, если меню открывается 
     $('nav li.has-sub > a').on('click', function () {
       var element = $(this).parent('li');
+      var id = element.attr('id');
       if (element.hasClass('open'))
-        sessionStorage.setItem(element.attr('id'),'has-sub');
+        sessionStorage.setItem(id,'has-sub');
       else 
-        sessionStorage.setItem(element.attr('id'),'has-sub open');
+        sessionStorage.setItem(id,'has-sub open');
+      // Код сварачивает все дочерние подменю если основное меню свернуто
+      element.find('*').each(function(index) {
+         if($(this).attr('class')=='has-sub open')
+            sessionStorage.setItem($(this).attr('id'),'has-sub');
+      });
     });
+  
     //Функция которая в случае нажатия на пункт меню делает его активным
     $('a').on('click', function () {
       var element = $(this).parent('li');
       var id = element.attr('id');
       sessionStorage.setItem('activate', id);
-    });   
+    }); 
+
     //Стилизация тега nav>ul>li.has-sub>a
     $('nav>ul>li.has-sub>a').append('<span class="holder"></span>');
 });
